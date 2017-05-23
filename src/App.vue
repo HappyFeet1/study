178ab6fd11d773af751afbb6a742e2ae7d09c496
何世孟 <hesmmf@hehenian.com>
2017-05-23 15:30:38 +0800

    路由动画

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
-      <transition :name="pageDirection">
-        <router-view class="child-view"></router-view>
-      </transition>
+    <transition :name="pageDirection">
+      <router-view class="child-view"></router-view>
+    </transition>
   </div>
 </template>
 <script>
-import {mapState, mapMutations} from 'vuex';
+import { mapState, mapMutations } from 'vuex';
 export default {
   name: 'app',
-  created(){
+  created() {
   },
-  computed:{
+  computed: {
     ...mapState([
       'pageDirection'
     ])
@@ -20,25 +20,66 @@ export default {
 </script>
 
 <style>
-.fade-enter-active, .fade-leave-active {
+html,
+body,
+#app {
+  position: relative;
+  height: 100%;
+  overflow: hidden;
+}
+
+.child-view {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  background: #fff;
+  overflow-y: auto;
+  -webkit-overflow-scrolling: touch;
+}
+
+.fade-enter-active,
+.fade-leave-active {
   transition: opacity .3s ease;
 }
-.fade-enter, .fade-leave-active {
+
+.fade-enter,
+.fade-leave-active {
   opacity: 0
 }
+
 .child-view {
-  transition: all .3s cubic-bezier(.55,0,.1,1);
+  transition: all .3s ease;
+  -webkit-backface-visibility: hidden;
+
+  backface-visibility: hidden;
+
+  -webkit-perspective: 1000;
+
+  perspective: 1000;
 }
-.slide-left-enter, .slide-right-leave-active {
-  opacity: 0;
-  -webkit-transform: translate(50px, 0);
-  transform: translate(50px, 0);
+
+.slide-left-enter,
+.slide-right-leave-active {
+  -webkit-transform: translate3d(100%, 0, 0);
+  transform: translate3d(100%, 0, 0);
 }
-.slide-left-leave-active, .slide-right-enter {
-  opacity: 0;
-  -webkit-transform: translate(-50px, 0);
-  transform: translate(-50px, 0);
+
+.slide-left-leave-active,
+.slide-right-enter {
+  opacity: .5;
+  -webkit-transform: translate3d(-20%, 0, 0);
+  transform: translate3d(-20%, 0, 0);
 }
+
+.slide-left-leave-active,
+.slide-right-enter-active {
+  z-index: 1;
+}
+
 .slide-right-leave-active,
-.slide-left-enter-active{position: absolute;}
+.slide-left-enter-active {
+  z-index: 2;
+}
 </style>
