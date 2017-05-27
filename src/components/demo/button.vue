e0458da8ca085aeb64839cc252413459a212360b
何世孟 <hesmmf@hehenian.com>
2017-05-27 17:37:37 +0800

    new

---
+++ src/components/demo/button.vue
@@ -0,0 +1,22 @@
+<template>
+   <div>
+        <mt-button type="default">
+           default</mt-button>
+       <mt-button type="primary">primary</mt-button>
+       <mt-button type="danger">danger</mt-button>
+   </div>
+</template>
+<script>
+import Vue from 'vue';
+// import '#/lib/button/';
+import Button from 'mint-ui/lib/button/';
+import 'mint-ui/lib/button/style.css'
+// Vue.component(Button.name, Button);
+export default {
+    components: {
+        'mt-button': Button
+    }
+}
+</script>
+
+
