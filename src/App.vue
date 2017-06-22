9ceb52c92d1cbead6bf029eeac144c7fee9c9971
何世孟 <hesmmf@hehenian.com>
2017-06-22 17:32:04 +0800

    优化路由动画

--- src/App.vue
+++ src/App.vue
@@ -57,36 +57,80 @@ body,
   background: #fff;
   overflow-y: auto;
   -webkit-overflow-scrolling: touch;
+  backface-visibility: hidden;
+  -webkit-backface-visibility: hidden;
+  perspective: 1000;
+  -webkit-perspective: 1000;
   background-color: #f2f2f2;
 }
 .fade-enter-active,
 .fade-leave-active {
-  transition: opacity .3s ease;
+  transition: opacity .4s ease;
 }
-.fade-enter,
-.fade-leave-active {
-  opacity: 0
-}
-.child-view {
-  transition: all .3s ease;
-  backface-visibility: hidden;
-  perspective: 1000;
-}
-.slide-left-enter,
-.slide-right-leave-active {
-  transform: translate3d(100%, 0, 0);
-}
-.slide-left-leave-active,
-.slide-right-enter {
-  opacity: .5;
-  transform: translate3d(-20%, 0, 0);
-}
-.slide-left-leave-active,
-.slide-right-enter-active {
+/*路由前进，退出*/
+.slide-left-leave-active{
+  animation: pageFromCenterToLeft 400ms forwards;
   z-index: 1;
 }
-.slide-right-leave-active,
-.slide-left-enter-active {
-  z-index: 2;
+/*路由后退，进入*/
+.slide-right-enter-active{
+  animation: pageFromLeftToCenter 400ms forwards;
+  z-index: 1;
+}
+/*路由后退，退出*/
+.slide-right-leave-active{
+  animation: pageFromCenterToRight 400ms forwards;
+  z-index: 10;
+}
+/*路由前进，进入*/
+.slide-left-enter-active{
+  animation: pageFromRightToCenter 400ms forwards;
+  z-index: 10;
+}
+
+/*-------------------------------*/
+/*路由前进，进入*/
+@keyframes pageFromRightToCenter {
+  from {
+    transform: translate3d(100%, 0, 0);
+    opacity: .9;
+  }
+  to {
+    transform: translate3d(0, 0, 0);
+    opacity: 1;
+  }
+}
+/*路由前进，退出*/
+@keyframes pageFromCenterToLeft {
+  from {
+    opacity: 1;
+    transform: translate3d(0, 0, 0);
+  }
+  to {
+    opacity: 0.5;
+    transform: translate3d(-20%, 0, 0);
+  }
+}
+/*路由后退，进入*/
+@keyframes pageFromLeftToCenter {
+  from {
+    opacity: .5;
+    transform: translate3d(-20%, 0, 0);
+  }
+  to {
+    opacity: 1;
+    transform: translate3d(0, 0, 0);
+  }
+}
+/*路由后退，退出*/
+@keyframes pageFromCenterToRight {
+  from {
+    transform: translate3d(0, 0, 0);
+    opacity: 1;
+  }
+  to {
+    transform: translate3d(100%, 0, 0);
+    opacity: .9;
+  }
 }
 </style>
